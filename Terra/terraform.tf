b0edc0bf159f5c1995ecc5dc6df11b2ef66c8ceb
ayo4tops01 <67613874+ayo4tops01@users.noreply.github.com>
2020-08-12 02:04:49 -0400

    commit

--- Terra/terraform.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-    backend "azurerm" {
-        resource_group_name = "ayRG"
-        storage_account_name = "storageter"
-        container_name       = "tfstate"
-        key                  = "web.tfstate"
-    }
-}
